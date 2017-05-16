06cfeb1c2237b5504792d6fb61848e28bfa46a6b
Joao Paulo Magalhaes <dev@jpmag.me>
2017-05-16 13:07:13 +0100

    enum name offsets seem to be working

--- examples/e2str/gen/main.cpp
+++ examples/e2str/gen/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 #include "myenum.hpp"
+#include "myenum.gen.hpp"
 #include "bitmask.hpp"
 
 // a googletest stub
@@ -11,21 +12,23 @@
         error_status |= !ok;                                    \
         if(!ok)                                                 \
         {                                                       \
-            std::cerr << "expected " #expr1 "=" << expr1        \
+            std::cerr << __FILE__ << ":" << __LINE__ << ": "    \
+                      << "expected " #expr1 "=" << expr1        \
                       << " to be equal to " #expr2 "=" << expr2 \
                       << "\n";                                  \
         }                                                       \
     }
-#define EXPECT_STR_EQ(expr1, expr2)                             \
-    {                                                           \
-        bool ok = (strcmp(expr1, expr2) == 0);                  \
-        error_status |= !ok;                                    \
-        if(!ok)                                                 \
-        {                                                       \
-            std::cerr << "expected " #expr1 "=" << expr1        \
-                      << " to be equal to " #expr2 "=" << expr2 \
-                      << "\n";                                  \
-        }                                                       \
+#define EXPECT_STR_EQ(expr1, expr2)                              \
+    {                                                            \
+        bool ok = (strcmp(expr1, expr2) == 0);                   \
+        error_status |= !ok;                                     \
+        if(!ok)                                                  \
+        {                                                        \
+            std::cerr << __FILE__ << ":" << __LINE__ << ": "     \
+                      << "expected " #expr1 "=" << expr1         \
+                      << " to be equal to " #expr2 "=" << expr2  \
+                      << "\n";                                   \
+        }                                                        \
     }
 static int error_status = 0;
 
@@ -114,9 +117,33 @@ void test_bm2str()
 
 int main(int argc, const char* argv[])
 {
-    test_e2str< MyEnum >();
+    EXPECT_EQ(eoffs_cls< MyEnum >(), 0);
+    EXPECT_EQ(eoffs_pfx< MyEnum >(), 0);
+
+    EXPECT_EQ(eoffs_cls< MyEnumClass >(), strlen("MyEnumClass::"));
+    EXPECT_EQ(eoffs_pfx< MyEnumClass >(), 0);
+
+    EXPECT_EQ(eoffs_cls< MyBitmask >(), 0);
+    EXPECT_EQ(eoffs_pfx< MyBitmask >(), strlen("BM_"));
+
+    EXPECT_EQ(eoffs_cls< MyBitmaskClass >(), strlen("MyBitmaskClass::"));
+    EXPECT_EQ(eoffs_pfx< MyBitmaskClass >(), strlen("MyBitmaskClass::BM_"));
+
+    EXPECT_EQ((int)esyms< MyEnumClass >().get("MyEnumClass::FOO")->value, (int)MyEnumClass::FOO);
+    EXPECT_EQ((int)esyms< MyEnumClass >().get("FOO")->value, (int)MyEnumClass::FOO);
+
+    EXPECT_EQ((int)esyms< MyBitmask >().get("BM_FOO")->value, (int)MyBitmask::BM_FOO);
+    EXPECT_EQ((int)esyms< MyBitmask >().get("FOO")->value, (int)MyBitmask::BM_FOO);
+
+    EXPECT_EQ((int)esyms< MyBitmaskClass >().get("MyBitmaskClass::BM_FOO")->value, (int)MyBitmaskClass::BM_FOO);
+    EXPECT_EQ((int)esyms< MyBitmaskClass >().get("BM_FOO")->value, (int)MyBitmaskClass::BM_FOO);
+    EXPECT_EQ((int)esyms< MyBitmaskClass >().get("FOO")->value, (int)MyBitmaskClass::BM_FOO);
+
     test_e2str< MyEnumClass >();
+    test_e2str< MyEnum >();
+
     test_bm2str< MyBitmask >();
     test_bm2str< MyBitmaskClass >();
+
     return error_status;
 }
